4bf31a1b7e974a84bef1a2a2c2ce0891f7d11c0c
PaulCari <pcaril@unsa.edu.pe>
2026-05-16 03:34:29 -0500

    Implementacion de los metodos que realizan las operaciones

---
+++ LAB05/PROPUESTOS/Calculadora/CalculatorImpl.java
@@ -0,0 +1,27 @@
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+
+public class CalculatorImpl extends UnicastRemoteObject implements Calculator {
+
+    public CalculatorImpl() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public double multiply(double a, double b) throws RemoteException {
+        return a * b;
+    }
+
+    @Override
+    public double divide(double a, double b) throws RemoteException {
+        if (b == 0) {
+            throw new ArithmeticException("Error: No se puede dividir entre cero.");
+        }
+        return a / b;
+    }
+
+    @Override
+    public double power(double a, double b) throws RemoteException {
+        return Math.pow(a, b); 
+    }
+}
